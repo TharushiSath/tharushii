c9db56940581c0c894bf9bde5549a8efb4667c24
TharushiSath <tharushikasathsarani98@gmail.com>
2024-02-07 18:34:45 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,263 @@
+export const Bio = {
+    name: "THARUSHIKA SATHSARANI",
+    roles: [
+      "FrontEnd Developer",
+      "BackEnd Developer",
+      
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/TharushiSath",
+    resume:"https://drive.google.com/file/d/1aVV2a8Ke-QKixAsIetLBLMavXi1V8K1W/view?usp=drive_link",
+    linkedin: "www.linkedin.com/in/tharushi-sath",
+   
+  };
+  
+  export const skills = [
+
+    {
+      title: "Languages",
+      skills: [
+        {
+          name: "Java",
+          image: "https://1000logos.net/wp-content/uploads/2020/09/Java-Logo.png",
+        },
+        
+        
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "C++",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1822px-ISO_C%2B%2B_Logo.svg.png",
+        },
+        
+        {
+          name: "Assembly",
+          image:
+            "https://static.thenounproject.com/png/762420-200.png",
+        },
+        
+      ],
+    },
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        
+        
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        
+       
+       
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        
+        
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+        
+      ],
+    },
+   
+    {
+      title: "Others",
+      skills: [
+       
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Net beans",
+          image:
+            "https://www.jrebel.com/sites/default/files/image/2021-09/social-blog-netbeans-updates%20%281%29.jpg",
+        },
+       
+      ],
+    },
+  ];
+  
+  
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://upload.wikimedia.org/wikipedia/en/a/a0/EUSL_logo2.png",
+      school: "Trincomalee Campus,Eastern University,SriLanka",
+      date: "Aug 2020 - Present",
+      grade: "3.45 GPA  (3.1 Sem)",
+      desc: "I am currently pursuing a Bachelor's degree in Computer Science at Trincomalee Campus,Eastern University. I have completed 5 semesters and have a GPA of 3.78. I have taken courses in Data Structures & Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.  I am learning and working on exciting projects with a team of talented developers.",
+      degree: "Bachelor of Computer Science ",
+    },
+    {
+      id: 1,
+      img: "https://upload.wikimedia.org/wikipedia/commons/8/83/ESOFT_Logo_1000_x_450_RGB.jpg",
+      school: "Esoft Metro Campus",
+      date: "Apr 2019 - Apr 2020",
+      grade:"Merit Pass",
+      desc: "I completed a Diploma in English at Esoft Metro Campus.",
+      
+    },
+    {
+      id: 0,
+      img: "https://img1.wsimg.com/isteam/ip/90a8d75f-13f8-4d2d-bdb2-6b1956d86374/IMG_5157-0002.JPG/:/cr=t:0%25,l:0%25,w:100%25,h:100%25",
+      school: "Galahitiyawa Central College",
+      
+      date: "2009-2017",
+     
+      desc: "I have done my GCE O/L and GCE A/L at Galahitiyawa Central College",
+      
+    },
+    
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "Weather-app",
+      
+      description:
+        "A simple and responsive weather app built with HTML and CSS. Get real-time weather updates for any location with an intuitive user interface. Stay informed about the current weather conditions, temperature, and more, all in a beautifully designed web application.",
+      image:
+        "https://user-images.githubusercontent.com/148171100/280450967-573d156b-4d49-415d-bd69-19716d62b588.png",
+      tags: [
+        
+        "HTML",
+        "CSS",
+        
+       
+      ],
+      category: "web app",
+      github: "https://github.com/TharushiSath/Weather--App",
+      
+    },
+    {
+      id: 0,
+      title: "To-DO-LIST",
+      
+      description:
+        "Developed a feature-rich to-do list app using React to boost productivity and streamline task management. #React #ProductivityApp #TaskManagement.",
+      image:
+        "https://user-images.githubusercontent.com/148171100/280450831-1678fed6-8c15-4fef-a674-7912da30c166.png",
+      tags: ["React Js"],
+      category: "web app",
+      github: "https://github.com/TharushiSath/To-Do-List",
+      
+      
+    },
+    {
+      id: 1,
+      title: "EDUFORD",
+      
+      description:
+        "Welcome to University Web App Frontend! This contains the HTML and CSS code for our user-friendly web application designed to enhance the university experience. Explore the code to see how I've created a sleek and intuitive user interface.",
+       
+        image: "https://user-images.githubusercontent.com/148171100/280450198-29915872-2b16-4140-a8db-de3b7fefb126.png",
+
+      tags: [
+       
+        "HTML",
+        
+        "CSS",
+        
+      ],
+      category: "web app",
+      github: "https://github.com/TharushiSath/University-web",
+      
+    },
+   
+    {
+      id: 3,
+      title: "School Management System",
+      
+      description:"School Management System with a GUI using Java for the front end, NetBeans IDE for development, and MySQL for the backend database.",
+      image:
+        "https://user-images.githubusercontent.com/148171100/283089598-1a1cd54c-bc66-4ff0-98e1-f7285744e39e.png",
+      tags: ["Java", "MySql"],
+      category: "Fullstack",
+      github: "https://github.com/TharushiSath/School-Management-System",
+      
+    },
+    
+    {
+      id: 8,
+      title: "Personalized Stress Level Prediction Via Sleep Habits Using Machine Learning",
+      date: "Currently working on",
+      description:
+        "A Stress Level Prediction python app made with Flask. It uses sleep habits to predict stress level.",
+      image:
+        "https://user-images.githubusercontent.com/148171100/283094988-7940ce4f-5c88-4810-9183-ffcdbdcafdbe.png",
+      tags: ["Python", "Flask", "HTML","CSS"],
+      category: "machine learning",
+     
+    },
+
+    {
+      id: 2,
+      title: "Real-estate-marketplace",
+      
+      description:
+        "Welcome to our Real Estate Marketplace,  Our platform offers a seamless experience for users to browse, buy, sell, and rent properties. ",
+      image:
+        "https://github.com/TharushiSath/Real-estate-market-place/assets/148171100/9446f844-3786-45b5-9637-154b8d65e790",
+      tags: ["React.js", "Express.js", "Node.js","MongoDB"],
+      category: "Fullstack",
+      github: "https://github.com/TharushiSath/Real-estate-market-place",
+     
+    },
+
+    {
+      id: 4,
+      title: "E-commerce website",
+      
+      description:
+        "Introducing our cutting-edge E-commerce platform, Dive into a world of seamless shopping experience",
+      image:
+        "https://drive.google.com/file/d/17a1QO9LuUESNtXO0NHx6u9hJ0ocdJhnf/view?usp=drive_link",
+      tags: ["React.js", "Express.js", "Node.js","MongoDB"],
+      category: "Fullstack",
+      
+     
+    },
+
+  ];
+  
+ 
